43150b0a0e53d0dcf5bff24dccb17eb28dec8806
adrian <profilte82@gmail.com>
2022-07-19 18:20:26 +0100

    Done

--- api/routes/trophyRouter.js
+++ api/routes/trophyRouter.js
@@ -1,24 +1,6 @@
 const router = require('express').Router()
-<<<<<<< HEAD
-
-const { 
-    checkAuth, 
-    checkRolUser, 
-    checkRolTeamLeader, 
-    checkRolOrganizer 
-} = require('../utils')
-
-const { 
-    createTrophy, 
-    updateTrophy, 
-    deleteTrophy, 
-    showAllTrophies, 
-    showTrophyById 
-} = require('../controllers/trophyController')
-=======
 const { checkAuth, checkRolUser, checkRolTeamLeader, checkRolOrganizer } = require('../utils')
 const { createTrophy, updateTrophy, deleteTrophy, showAllTrophies, showTrophyById, showTrophyImage } = require('../controllers/trophyController')
->>>>>>> af3749f7b6b19b7ea13497f64f489c89ee9a5913
 
 router
     .post('/', checkAuth, checkRolOrganizer, createTrophy)
